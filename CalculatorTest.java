6b01a161baf9d7a477e25c68bafb3285ccc92a75
Anton Ivanov <antonstal@yandex.ru>
2023-10-01 22:35:23 +0300

    add Action  with install Jupiter and AssertJ v8.6

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -19,32 +19,32 @@ public class CalculatorTest {
         calculator = new Calculator();
     }
 
-    @ParameterizedTest
+    @Test
     @CsvSource({"5, 2, 7", "5, 0, 5", "-2, 6, 4", "5, -2, 3", "-5, -2, -7", "5.5, 2, 7.5"})
     public void testSummarize(double number1, double number2, double expectedResult) {
         assertEquals(expectedResult, calculator.summarize(number1, number2));
     }
 
-    @ParameterizedTest
+    @Test
     @CsvSource({"5, 2, 3", "0, 2, -2", "6, 0, 6", "5, 7, -2", "-5, -2, -3", "5.5, 2, 3.5"})
     public void testSubtract(double number1, double number2, double expectedResult) {
         assertEquals(expectedResult, calculator.subtract(number1, number2));
     }
 
-    @ParameterizedTest
+    @Test
     @CsvSource({"5, 2, 10", "0, 2, 0", "-5, -2, 10", "5.5, 2, 11"})
     public void testMultiply(double number1, double number2, double expectedResult) {
         assertEquals(expectedResult, calculator.multiply(number1, number2));
 
     }
 
-    @ParameterizedTest
+    @Test
     @CsvSource({"6, 2, 3", "0, 2, 0", "11, 2, 5.5", "-10, -2, 5", "9, -3, -3"})
     public void testDivide(double number1, double number2, double expectedResult) {
         assertEquals(expectedResult, calculator.divide(number1, number2));
     }
 
-    @ParameterizedTest
+    @Test
     @CsvSource({"2, 0"})
     public void testDivideByZero(double number1, double number2) {
         assertThatExceptionOfType(ArithmeticException.class)
@@ -54,13 +54,13 @@ public class CalculatorTest {
                 .withMessage("can't divide by zero");
     }
 
-    @ParameterizedTest
+    @Test
     @ValueSource(strings = {"0", "5", "6.2", "-5.3"})
     public void testIsNumberTrue(String value) {
         assertTrue(calculator.isNumber(value), "IsNumber works incorrect for values");
     }
 
-    @ParameterizedTest
+    @Test
     @NullAndEmptySource
     @ValueSource(strings = {" ", "?", "пять", "\t", "\n"})
     public void testIsNumberFalse(String value) {
